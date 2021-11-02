9a3f7b843a5bdfe917e3f024ffe80c3071f87472
cc710788@gmail.com <cc710788@gmail.com>
2021-11-02 18:26:25 +0530

    last

--- src/components/Books.js
+++ src/components/Books.js
@@ -7,6 +7,7 @@ const Books = () => {
       const tours= await axios({
         method:'get',
         url:'https://infinite-spire-90765.herokuapp.com/webhook-checkout',
+        withCredentials:true,
         
       })
       setTourss(tours)
