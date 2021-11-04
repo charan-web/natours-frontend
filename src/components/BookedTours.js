3b1e6c46564418f477c07aa65164726e380dcae5
cc710788@gmail.com <cc710788@gmail.com>
2021-11-04 10:09:43 +0530

    last

---
+++ src/components/BookedTours.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Tours from "./Tours"
+axios.defaults.headers['authorization'] = `Bearer ${localStorage.getItem('jwt')}`
+const BookedTours = () => {
+    const [tourss,setTourss] = useState([])
+  async  function bookings(){
+      const tours= await axios({
+        method:'get',
+        url:'https://infinite-spire-90765.herokuapp.com/booking/my-tours',
+        // withCredentials:true,
+        // headers: {
+        //   'Authorization':  `Bearer ${localStorage.getItem('jwt')}`
+        // }
+      })
+      setTourss(tours)
+     
+    }
+   
+
+    useEffect(()=>{
+        bookings()
+        // async function getours
+    },[])
+    return ( 
+    <>
+     {tourss.length===0? <h1>No tours</h1>:
+     <>
+      {tourss.map(el=>{
+         return <Tours key={el}/>
+      })}  </>}
+    </> 
+    );
+}
+ 
+export default BookedTours;
